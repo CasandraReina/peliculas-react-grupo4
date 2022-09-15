5aaa34d81016f0b9a08fdbd18f62bb229e451ff4
Casandra Reina <casandramailen.0010@gmail.com>
2022-09-14 21:59:47 -0300

    modificacion

--- src/Peliculas.js
+++ src/Peliculas.js
@@ -6,22 +6,22 @@ class Peliculas extends React.Component{
 <section className="Recientes">
   <div className="PeliculasRecientes">
 
-  <article class="Peliculas">
-     <img src={this.props.img} alt="" class="Icono"/>
+  <article className="Peliculas">
+     <img src={this.props.img} alt="" className="Icono"/>
     <ul>
-     <h1 class="TituloPelicula">{this.props.titulo}</h1>
-     <p class="Genero">{this.props.genero}</p>
-     <h6 class="Puntuacion">Puntuacion  
-     <i class="heart fa-solid fa-heart"></i>
-     <i class="heart fa-solid fa-heart"></i>
-     <i class="heart fa-solid fa-heart"></i>
-     <i class="heart fa-solid fa-heart"></i>
-     <i class="heart fa-regular fa-heart"></i></h6>
+     <h1 className="TituloPelicula">{this.props.titulo}</h1>
+     <p className="Genero">{this.props.genero}</p>
+     <h6 className="Puntuacion">Puntuacion  
+     <i className="heart fa-solid fa-heart"></i>
+     <i className="heart fa-solid fa-heart"></i>
+     <i className="heart fa-solid fa-heart"></i>
+     <i className="heart fa-solid fa-heart"></i>
+     <i className="heart fa-regular fa-heart"></i></h6>
      <h3>Descripcion</h3>
-     <p class="Descripcion">{this.props.descripcion}</p>
+     <p className="Descripcion">{this.props.descripcion}</p>
 
-     <a class="VerTrailer" href={this.props.trailer}>Ver Trailer</a>
-     <a class="VerTrailer" href="#">Mas Detalles</a>
+     <a className="VerTrailer" href={this.props.trailer}>Ver Trailer</a>
+     <a className="VerTrailer" href="#">Mas Detalles</a>
   </ul>
 </article>
 </div> </section>
